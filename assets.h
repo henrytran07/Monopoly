665acd79e048e502a755650b3358aa95cb7ca1bc
Henry Tran <tranhuuloc@Henry-Tran.local>
2024-07-15 13:22:57 -0700

    Uploaded assets file

---
+++ assets.h
@@ -0,0 +1,16 @@
+
+#ifndef ASSETS_H
+#define ASSETS_H
+
+#include <iostream> 
+
+using namespace std; 
+struct House; 
+struct Hotel; 
+class Properties; 
+class Money; 
+class Assets {
+    
+};
+
+#endif 
